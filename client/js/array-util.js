a495b86901cd43535033645130ff9769171a79c0
Yuki Omori <yuki.omori@inte.co.jp>
2017-05-04 10:25:18 +0900

    set up jest test environment

---
+++ client/js/array-util.js
@@ -0,0 +1,8 @@
+const getRange = function(fromNum, toNum) {
+  return Array.from({ length: toNum - fromNum + 1　},
+  	(unused, i)  => i + fromNum);
+};
+
+module.exports = {
+	getRange: getRange
+};
