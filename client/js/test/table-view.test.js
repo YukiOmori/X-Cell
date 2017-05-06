39c4784cff97ca643f615fdaad510f67239039fa
Yuki Omori <yuki.omori@inte.co.jp>
2017-05-06 15:30:21 +0900

    add formula bar and highlight

--- client/js/test/table-view.test.js
+++ client/js/test/table-view.test.js
@@ -11,6 +11,39 @@ describe('table-view', ()=> {
   });
 
   describe('table body', () => {
+
+    it('updates FROM the value of the current cell', () => {
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      model.setValue({ col: 2, row: 1}, '123');
+      view.init();
+
+      const formulaBarEl = document.querySelector('#formula-bar')
+      expect(formulaBarEl.value).toBe('');
+
+      const trs = document.querySelectorAll('TBODY TR');
+      trs[1].cells[2].click();
+
+      expect(formulaBarEl.value).toBe('123');
+
+    });
+
+    it('hightlights the current cell when clicked', () => {
+      const model = new TableModel(10, 5);
+      const view = new TableView(model);
+      view.init();
+
+      let trs = document.querySelectorAll('TBODY TR');
+      let td = trs[2].cells[3];
+      expect(td.className).toBe('');
+
+      td.click();
+
+      trs = document.querySelectorAll('TBODY TR');
+      td = trs[2].cells[3];
+      expect(td.className).not.toBe('');
+    });
+
     it('has the right size', () => {
       const numCols = 6;
       const numRows = 10;
